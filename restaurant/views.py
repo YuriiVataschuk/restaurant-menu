09682dc3b5c1e2fea895598a82ccf037307e6e0f
yura.vataschuk <yura.vataschuk@gmail.com>
2023-07-02 17:51:02 +0200

    Pagination fixed

--- restaurant/views.py
+++ restaurant/views.py
@@ -78,7 +78,7 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
-    paginate_by = 5
+    paginate_by = 15
 
     def get_context_data(self, *, object_list=None, **kwargs) -> dict:
         context = super(DishListView, self).get_context_data(**kwargs)
